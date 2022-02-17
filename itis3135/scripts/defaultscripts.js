1a748d7d0f7df1ab6e2b079c7e189829f2d4fbfa
cgembala <97686122+cgembala@users.noreply.github.com>
2022-02-17 17:06:36 -0500

    new page and scritps

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -11,17 +11,21 @@ function showTime()
     second = now.getSeconds(),
     period = "AM";
 
-  if (hour >= 12) {
+  if (hour >= 12) 
+  {
     period = "PM";
   }
-  if (hour == 0) {
+  if (hour == 0) 
+  {
     hour = 12;
   }
-  if (hour > 12) {
+  if (hour > 12) 
+  {
     hour = hour - 12;
   }
 
-  Number.prototype.pad = function(digits) {
+  Number.prototype.pad = function(digits) 
+  {
     for (var n = this.toString(); n.length < digits; n = 0 + n);
     return n;
   }
@@ -34,7 +38,8 @@ function showTime()
     document.getElementById(ids[i]).firstChild.nodeValue = values[i];
 }
 
-function initClock() {
+function initClock() 
+{
   showTime();
   window.setInterval("showTime()", 1);
 }
@@ -101,3 +106,5 @@ function MilestoKilo()
   let newmile = kilo.toFixed(2);
   alert("The distance in kilometers is: " + newmile);
 }
+
+  
---
+++ itis3135/scripts/polygons.js
@@ -0,0 +1,86 @@
+var sides;
+var shape;
+var polygon;
+var rerun;
+
+function getSides() 
+{
+  sides = prompt("The Cyan Gator would like you to enter the number of sides for a polygon from 1 to 10.");
+  sides = parseFloat(sides);
+  return sides;
+}
+
+function errorRetry() 
+{
+  sides = prompt("You have entered an invalid number please chose from 1 to 10.");
+  sides = parseFloat(sides);
+  return sides;
+}
+
+function validEntry() 
+{
+  if (shape > 10 || shape <= 0) 
+  {
+    errorRetry();
+  }
+}
+
+function getShape() 
+{
+  rerun = 1;
+  while (rerun == 1) 
+  {
+    getSides();
+    var shape = Math.round(Math.abs(sides));
+    validEntry();
+    while (sides <= 10) 
+    {
+      if (shape == 1) 
+      {
+        polygon = "hexagon";
+      } else if (shape == 2) 
+      {
+        polygon = "digon";
+      } else if (shape == 3) 
+      {
+        polygon = "trigon";
+      } else if (shape == 4) 
+      {
+        polygon = "tetragon";
+
+      } else if (shape == 5) 
+      {
+        polygon = "pentagon";
+
+      } else if (shape == 6) 
+      {
+        polygon = "hexagon";
+
+      } else if (shape == 7) 
+      {
+        polygon = "heptagon";
+
+      } else if (shape == 8) 
+      {
+        polygon = "octagon";
+
+      } else if (shape == 9) 
+      {
+        polygon = "enneagon";
+
+      } else if (shape == 10) 
+      {
+        polygon = "decagon";
+      }
+      alert("Number of sides = " + shape + "\n" + "Name of the polygon is " + polygon);
+      break;
+    }
+    rerun = prompt("Do you want to go again? Enter 1 for yes or 2 for no");
+    rerun = parseFloat(rerun);
+  }
+
+}
+
+ 
+
+
