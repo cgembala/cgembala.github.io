8a9497d91629a2144d2079dd1f19a5723fdce902
cgembala <97686122+cgembala@users.noreply.github.com>
2022-04-11 20:49:57 -0400

    new activities and peer review

---
+++ itis3135/Updated-Activity12/Untitled-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char source[] = "itis3200_homework3";
+    char destination[10];
+    strcpy(destination, source);
+
+    return 0;
+
+
+}
